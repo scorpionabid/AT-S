98c1072323e226c1b2b7f2a34868df3802311a85
Abid Aghayev <106526739+scorpionabid@users.noreply.github.com>
2025-07-26 06:50:53 +0400

    header

--- frontend/src/components/layout/StandardPageLayout.tsx
+++ frontend/src/components/layout/StandardPageLayout.tsx
@@ -52,7 +52,7 @@ const StandardPageLayout: React.FC<StandardPageLayoutProps> = ({
         </div>
 
         {/* Page Content */}
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-6 pb-8">
           {children}
         </div>
       </div>
